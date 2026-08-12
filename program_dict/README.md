3e929952cbff53e5aa04d88525a77abf409d678e
Zakharik-Students <zakharamockin@icloud.com>
2026-08-12 16:58:07 +0300

    Added 'show table function' and created 'README.md'

---
+++ program_dict/README.md
@@ -0,0 +1,7 @@
+My personal CLI dictionary for programming education.
+
+Tasks:
+    1) Make file with dictionary
+    2) Make beautiful output
+    3) Add remove function
+    4) Check github and modificate my programm
--- program_dict/program_dict.py
+++ program_dict/program_dict.py
@@ -3,6 +3,18 @@ main_dict = dict() #make dict in saved file
 print("\n", "Hello! It's your dictionary".center(80))
 print("Press 'F' to get help".center(80))
 
+def get_table(main_dict):
+    headers = ["word", "translation"]
+    header_format = "{:<30} {:<30}"
+    row_format = "{:<30} {:<30}"
+    
+    print()
+    print( header_format.format(*headers))
+    print('-' * 60)
+    for word, translate in main_dict.items():
+        print(row_format.format(word, translate))
+
+
 def add_funct(word, translate, main_dict):
     main_dict[word] = translate
     print(main_dict)
@@ -13,7 +25,8 @@ def get_funct(word, main_dict):
 def get_help():
     print("\n", "add <word> <translate>".center(80),
           "get <word>".center(80),
-          "'q' - quit programm".center(80))
+          "'q' - quit programm".center(80),
+          "'t' - get table".center(80))
 
 while True:
     enter = input().lower().split()
@@ -23,6 +36,8 @@ while True:
         get_funct(enter[1], main_dict)
     elif enter[0] == "f":
         get_help()
+    elif enter[0] == "t":
+        get_table(main_dict)
     elif enter[0] == 'q':
         break
     else:
